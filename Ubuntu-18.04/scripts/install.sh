983f5a4447ec41038c2c9cf2c4c1adafe3335ff8
András Szabácsik <andras@szabacsik.com>
2019-01-04 18:03:24 +0100

    Install VirtualBox Guest Additions

--- Ubuntu-18.04/scripts/install.sh
+++ Ubuntu-18.04/scripts/install.sh
@@ -1,1 +1,8 @@
 #!/bin/bash -eux
+
+# Install VirtualBox Guest Additions.
+cd /home/vagrant
+vboxVersion=`cat .vbox_version`
+mount VBoxGuestAdditions_"$vboxVersion".iso -o loop /mnt
+sh /mnt/VBoxLinuxAdditions.run
+umount /mnt
