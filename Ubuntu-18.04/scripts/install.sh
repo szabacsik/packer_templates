8b84abd0a17595a094eed1522def12d6a61d9b39
András Szabácsik <andras@szabacsik.com>
2019-01-05 21:47:28 +0100

    Install docker

--- Ubuntu-18.04/scripts/install.sh
+++ Ubuntu-18.04/scripts/install.sh
@@ -6,3 +6,10 @@ vboxVersion=`cat .vbox_version`
 mount VBoxGuestAdditions_"$vboxVersion".iso -o loop /mnt
 sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
+
+# Install Docker.
+apt install curl -y
+curl -fsSL https://get.docker.com -o get-docker.sh
+chmod +x get-docker.sh
+./get-docker.sh
+usermod -aG docker vagrant
