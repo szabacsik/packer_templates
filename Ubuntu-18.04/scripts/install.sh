1a2e94fbabeae92b32f312d0b0efd7eb754427fe
András Szabácsik <andras@szabacsik.com>
2019-09-02 12:16:18 +0200

    Add install docker-compose

--- Ubuntu-18.04/scripts/install.sh
+++ Ubuntu-18.04/scripts/install.sh
@@ -13,3 +13,9 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 chmod +x get-docker.sh
 ./get-docker.sh
 usermod -aG docker vagrant
+
+# Install Docker Compose.
+apt install jq -y
+COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
+sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+chmod 755 /usr/local/bin/docker-compose
